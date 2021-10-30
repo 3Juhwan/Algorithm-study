17e4904a111006c126892e207025d67a7451659f
3Juhwan <13selfesteem91@naver.com>
2021-10-30 07:34:32 +0000

    :bulb: Update Code

--- baekjoon/11053.py
+++ baekjoon/11053.py
@@ -1,3 +1,4 @@
+
 n = int(input())
 arr = list(map(int, input().split()))
 dp = [1] * n
@@ -7,3 +8,30 @@ for i in range(n):
             dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
+
+
+
+'''Recursion Code'''
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [-1] * n
+
+
+def LIS(x):
+    if x == n:
+        return 1
+
+    ret = dp[x]
+    if ret != -1:
+        return ret
+
+    for i in range(x+1, n):
+        if x == -1 or arr[x] < arr[i]:
+            ret = max(ret, LIS(i))
+
+    dp[x] = ret + 1
+    return dp[x]
+
+
+print(LIS(-1))
